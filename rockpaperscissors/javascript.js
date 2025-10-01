b908436c48bfc0775c306968896238520cf5b28c
Fixedmyback <hegeda97@gmail.com>
2025-10-01 02:13:15 +0200

    The game itself is working properly

--- rockpaperscissors/javascript.js
+++ rockpaperscissors/javascript.js
@@ -1,21 +1,63 @@
-const userScore = 0;
-const computerScore = 0;
-const userScore_span = document.getElementById("userScore");
-const computerScore_span = document.getElementById("computerScore");
+let userScore = 0;
+let computerScore = 0;
+const userScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("computer-score");
 const scoreBoard_div = document.querySelector(".score-board");
-const result_div = document.querySelector(".result");
+const result_p = document.querySelector(".result > p");
 const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
-
-console.log("hello");
 
 function getComputerChoice(){
     const choices = ['r', 'p', 's'];
     const randomNumber = Math.floor(Math.random() * 3);
     return choices[randomNumber];
 }
-console.log(getComputerChoice());
+
+function convertToWord(letter){
+    if (letter === "r") return "rock";
+    if (letter === "p") return "paper";
+    return "scissors";
+}
+
+function win(userChoice, computerChoice){
+    userScore++;
+    userScore_span.innerHTML = userScore;
+    computerScore_span.innerHTML = computerScore;
+    
+    result_p.innerHTML = `Your ${convertToWord(userChoice)} beats computer's ${convertToWord(computerChoice)}. You win!`;
+}
+
+function lose(userChoice, computerChoice){
+    computerScore++;
+    computerScore_span.innerHTML = computerScore;
+    result_p.innerHTML = `Computer's ${convertToWord(userChoice)} beats your ${convertToWord(computerChoice)}. You lost!`;
+}
+
+function draw(){
+    result_p.innerHTML = "It's a draw";
+}
+
+function game(userChoice){
+    const computerChoice = getComputerChoice();
+    switch (userChoice + computerChoice){
+        case "rs":
+        case "pr":
+        case "sp":
+            win(userChoice, computerChoice);
+            break;
+        case "sr":
+        case "rp":
+        case "ps":
+            lose(computerChoice, userChoice);
+            break;
+        case "rr":
+        case "pp":
+        case "ss":
+            draw(userChoice, computerChoice);
+            break;
+    }
+}
 
 
 function main() {
